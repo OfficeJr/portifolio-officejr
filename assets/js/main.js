905f25d899f3804814c8cdbda85d9882b99f1ecc
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-06-01 11:55:42 -0300

    u-0.04

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,9 @@ variable.nav.btnHeaderExpand.addEventListener('click', function () {
     variable.nav.header.classList.toggle("expanded");
     variable.nav.btnHeaderExpand.classList.toggle("fi-br-cross");
 })
+
+
+window.addEventListener("scroll", function (){
+    variable.nav.header.classList.toggle('scroll_header', window.scrollY > 0);
+})
+
