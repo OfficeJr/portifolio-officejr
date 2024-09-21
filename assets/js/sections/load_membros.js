c76abd64fc2caf41bd8e90b8010efb3f9cf8427c
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-09-21 16:26:40 -0300

    feat
    terminado o sistema de escolhas de membrois

--- assets/js/sections/load_membros.js
+++ assets/js/sections/load_membros.js
@@ -1,13 +1,15 @@
-
 function Card_Membros(data) {
-    const container = document.getElementById('marketing');
+    const container = document.querySelector('.container_selecione_acessoria');
+
+    container.innerHTML = '';
 
     data.forEach(item => {
         const div = document.createElement('div');
         div.className = 'container_acessoria';
 
         const img = document.createElement('img');
-        img.src = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s`; 
+        img.src = `assets/images/membros/${item.image}.JPG`;
+        img.alt = `Foto de ${item.name}`;
         div.appendChild(img);
 
         const name = document.createElement('h3');
@@ -15,20 +17,10 @@ function Card_Membros(data) {
         div.appendChild(name);
 
         const description = document.createElement('p');
-        description.textContent = `Assessor de ${item.accessory}`;
+        description.textContent = item.accessory;
         div.appendChild(description);
 
         container.appendChild(div);
     });
 }
 
-function loadDatas(){
-    fetch('assets/json/members.json')
-    .then(response => response.json())
-    .then(members => {
-        Card_Membros(members.marketing);
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
-}
-
-document.addEventListener('DOMContentLoaded', loadDatas);
--- assets/js/sections/select_membros.js
+++ assets/js/sections/select_membros.js
@@ -1,9 +1,19 @@
+
+function loadDatas() {
+    fetch('assets/json/members.json')
+        .then(response => response.json())
+        .then(members => {
+            membersData = members;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+}
+
 const select_acessoria = document.querySelector(".select_acessoria");
 const btn_select_acessoria = document.querySelector(".btn_select_acessoria");
 const select_acessoria_conteudo = document.querySelector(".select_acessoria_conteudo");
 const options_acessoria = document.querySelector(".options_acessorias");
 const icon = document.querySelector(".fi.fi-br-caret-down");
-const acessorias = Array('Financeiro', 'Marketing', 'Projetos', 'Rercusos Humanos', 'Comercial');
+const acessorias = Array('Diretoria', 'Finanças', 'Marketing', 'Projetos', 'Recursos Humanos');
 const acessoria_selecionada = document.querySelector(".acessoria_selecionada");
 
 btn_select_acessoria.addEventListener("click", () => {
@@ -12,30 +22,37 @@ btn_select_acessoria.addEventListener("click", () => {
     if (select_acessoria_conteudo.classList.contains("active")) {
         icon.style.transform = "rotate(180deg)";
     } else {
-
         icon.style.transform = "rotate(0)";
     }
 });
 
-var acessoria_atual = null;
+let acessoria_atual = null;
 
 function setAcessorias() {
     acessorias.forEach((acessoria) => {
         let li = document.createElement("li");
         li.textContent = acessoria;
         li.classList.add("li-text-membros");
-        options_acessoria.appendChild(li);
+
         li.onclick = () => {
-            acessoria_selecionada.textContent = li.textContent;
-            if (li.textContent == acessoria_selecionada.textContent) {
-                li.style.backgroundColor = "#1c263d9d";
-                if (acessoria_atual != null) {
-                    acessoria_atual.style.backgroundColor = "";
-                }
+            if (acessoria_atual) {
+                acessoria_atual.style.backgroundColor = "";
             }
+
+            acessoria_selecionada.textContent = li.textContent;
+            li.style.backgroundColor = "#1c263d9d";
             acessoria_atual = li;
-        }
+            
+            select_acessoria_conteudo.classList.remove("active")
+            icon.style.transform = "rotate(0)";
+
+            const acessoria_lower = li.textContent.toLowerCase(); 
+            Card_Membros(membersData[acessoria_lower]);
+        };
+
+        options_acessoria.appendChild(li);
     });
 }
 
 setAcessorias();
+loadDatas();
